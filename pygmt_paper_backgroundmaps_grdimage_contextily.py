b134bc6582bf2baf777251cbe9a079b429a738ac
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-08 22:30:10 +0100

    Combine panel b and c, add 3-D map via grdview in panel c

--- pygmt_paper_backgroundmaps_grdimage_contextily.py
+++ pygmt_paper_backgroundmaps_grdimage_contextily.py
@@ -16,29 +16,29 @@ with fig.subplot(
 
     # Top left
     fig.basemap(region=region, projection="M?", panel=0)
-    fig.coast(
-        land="lightgray",
-        water="lightblue",
-        shorelines="1/0.5p,gray30",
-        resolution="high",
-    )
-    with pygmt.config(MAP_SCALE_HEIGHT="10p"):
-        fig.basemap(map_scale="n0.86/0.1+c+w100k+f+l")
+    fig.coast(land="gray", water="lightblue", shorelines=True, resolution="high")
+    fig.basemap(map_scale="n0.86/0.1+c+w100k+f+l")
 
     # Top right
+    grd_geoid = pygmt.datasets.load_earth_geoid(resolution="03m", region=region)
     fig.basemap(region=region, projection="M?", panel=1)
-    grid_relief = pygmt.datasets.load_earth_relief(resolution="03m", region=region)
-    fig.grdimage(grid=grid_relief, cmap="SCM/oleron")
-    fig.colorbar(frame=["x+lsurface elevation", "y+lm"], position="JLM")
-    fig.coast(shorelines="1/0.5p,gray30", resolution="high")
+    fig.grdimage(grid=grd_geoid, cmap="SCM/lajolla")
+    fig.grdcontour(grid=grd_geoid)
+    fig.colorbar(frame=["x+lheight", "y+lm"], position="JLM")
+    fig.coast(shorelines="1/0.5p,white", resolution="high")
 
     # Bottom left
-    fig.basemap(region=region, projection="M?", panel=2)
-    grid_geoid = pygmt.datasets.load_earth_geoid(resolution="03m", region=region)
-    fig.grdimage(grid=grid_geoid, cmap="SCM/lajolla")
-    fig.grdcontour(grid=grid_geoid)
-    fig.colorbar(frame=["x+lheight", "y+lm"], position="JRM")
-    fig.coast(shorelines="1/0.5p,white", resolution="high")
+    grd_relief = pygmt.datasets.load_earth_relief(resolution="03m", region=region)
+    fig.basemap(region=region, projection="M?", perspective=[-150, 25], panel=2)
+    fig.grdview(
+        grid=grd_relief,
+        cmap="SCM/oleron",
+        surftype="s",
+        zsize="1.5c",
+        plane="-1500+ggrey",
+        perspective=True,
+    )
+    fig.colorbar(frame=["x+lsurface elevation", "y+lm"], position="jTC+h+ml")
 
     # Bottom right
     fig.tilemap(region=region, projection="M?", zoom=7, panel=3)
