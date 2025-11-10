59d50fe810b9b34edcb79fdce4ea86160ff59755
Dongdong Tian <seisman.info@gmail.com>
2025-11-10 16:16:29 +0800

    Update the Python script

--- 2.PyGMT_ecosystem.py
+++ 2.PyGMT_ecosystem.py
@@ -4,21 +4,20 @@ import pygmt
 red, blue = "238/86/52", "#4B8BBE"  # GMT red and Python blue
 
 fig = pygmt.Figure()
-fig.basemap(region=[-1, 16, -1, 7], projection="x1c", frame="+n")
+fig.basemap(region=[-1, 16, -1, 8], projection="x1c", frame="+n")
 
 # Add the GMT part.
-x1 = 1.0
-fig.plot(x=[x1 - 1.5, x1 + 1.5, x1 + 1.5, x1 - 1.5], y=[0, 0, 6, 6], pen="1p,black", close=True)
-fig.vlines(x=x1, ymin=1, ymax=3, pen="1p,black")
-fig.vlines(x=x1, ymin=3, ymax=5, pen="1p,black")
-fig.plot(x=[x1] * 3, y=[1, 3, 5], style="R2.1c/1c/3p", fill=red, pen="0.5p,black")
+fig.plot(x=[-0.5, 2.5, 2.5, -0.5], y=[0, 0, 6, 6], pen="1p,black", close=True)
+fig.vlines(x=1, ymin=1, ymax=3, pen="1p,black")
+fig.vlines(x=1, ymin=3, ymax=5, pen="1p,black")
+fig.plot(x=[1.0] * 3, y=[1, 3, 5], style="R2.1c/1c/3p", fill=red, pen="0.5p,black")
 fig.text(
-    x=[x1] * 4,
+    x=[1.0] * 4,
     y=[1.25, 0.75, 3, 5],
     text=["GMT low-level", "library", "GMT C API", "GMT modules"] ,
     font="8p,1,white",
 )
-fig.logo(position=f"g{x1}/7+jMC+w2c")
+fig.logo(position="g1/7+jMC+w2c")
 
 # Add the Python part.
 fig.plot(x=[7.5, 15.5, 15.5, 7.5], y=[0, 0, 6, 6], pen="1p,black", close=True)
@@ -54,15 +53,15 @@ fig.image(
 )
 
 # Add the PyGMT part.
-fig.pygmtlogo(position="g5/2.6+jMC+w2c", wordmark="vertical")
+fig.pygmtlogo(position="g5/2.65+jMC+w2c", wordmark="vertical")
 fig.plot(
-    data=[[2.5, 3, 3.75, 3], [6.25, 3, 7.5, 3]],
+    data=[[2.6, 3, 3.9, 3], [6.1, 3, 7.4, 3]],
     style="v0.4c+s+b+e+gblack",
     pen="2p",
 )
 
 fig.show()
-fig.savefig("Fig2.png")
+fig.savefig("Fig2.PyGMT_ecosystem.png")
 
 # %%
 
