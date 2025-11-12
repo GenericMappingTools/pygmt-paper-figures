3846e42f19c9c646f280bdab20a4e59f91cdfe80
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-12 23:52:56 +0100

    Reorder panels

--- Fig3_PyGMT_backgrounds.py
+++ Fig3_PyGMT_backgrounds.py
@@ -7,7 +7,7 @@ with fig.subplot(
     nrows=2,
     ncols=2,
     subsize=("12c", "8.5c"),
-    autolabel="(a)+o0.15c/0.3c+gwhite@30",  # maybe +v
+    autolabel="(a)+o0.15c/0.3c+gwhite@30+v",  # vertical order of labels
     margins=("0.3c", "0.2c"),
     sharex="b",
     sharey="l",
@@ -19,14 +19,14 @@ with fig.subplot(
     fig.coast(land="gray", water="lightblue", shorelines=True, resolution="high")
     fig.basemap(map_scale="n0.86/0.1+c+w100k+f+l")
 
+    # Bottom left
+    fig.tilemap(region=region, projection="M?", zoom=7, panel=1)
+
     # Top right
     grd_relief = pygmt.datasets.load_earth_relief(resolution="01m", region=region)
-    fig.basemap(region=region, projection="M?", panel=1)
+    fig.basemap(region=region, projection="M?", panel=2)
     fig.grdimage(grid=grd_relief, cmap="SCM/oleron", shading=True)
     fig.colorbar(frame=["x+lElevation", "y+lm"], position="JRM")
-
-    # Bottom left
-    fig.tilemap(region=region, projection="M?", zoom=7, panel=2)
 
     # Bottom right
     fig.basemap(region=region, projection="M?", perspective=(-150, 25), panel=3)
