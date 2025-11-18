6bdc2f729f1485c5384909e6a756fb32565a6ca0
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-18 12:36:20 +0100

    Change from japan to myanmar

--- Fig4_PyGMt_pandas.py
+++ Fig4_PyGMt_pandas.py
@@ -60,4 +60,80 @@ with fig.inset(
     )
 
 fig.show()
-fig.savefig("Fig4_PyGMT_pandas.png")
+fig.savefig("Fig4_PyGMT_pandas_japan.png")
+
+
+
+# %%
+import io
+
+import pygmt
+import pandas as pd
+import requests
+
+lon_min = 75
+lon_max = 115
+lat_min = 5
+lat_max = 35
+mag_min = 4
+mag_max = 9
+start_time = "2024-01-01"
+end_time = "2025-10-30"
+
+url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
+params = {
+    "format": "csv",
+    "starttime": start_time,
+    "endtime": end_time,
+    "minlatitude": lat_min,
+    "maxlatitude": lat_max,
+    "minlongitude": lon_min,
+    "maxlongitude": lon_max,
+    "minmagnitude": mag_min,
+    "maxmagnitude": mag_max,
+}
+r = requests.get(url, params=params)
+df_eqs = pd.read_csv(io.StringIO(r.text))
+
+fig = pygmt.Figure()
+
+fig.basemap(region=[lon_min, lon_max, lat_min, lat_max], projection="M15c", frame=True)
+fig.coast(land="gray95", shorelines="gray50")
+
+# Plot epicenters with color (hypocentral depth) or size (moment magnitude)
+pygmt.makecpt(cmap="SCM/navia", series=[0, 200], reverse=True)
+fig.colorbar(frame=["xaf+lHypocentral depth", "y+lkm"], position="+ef0.3c")
+fig.plot(
+    x=df_eqs.longitude,
+    y=df_eqs.latitude,
+    size=0.01 * 2**df_eqs.mag,
+    fill=df_eqs.depth,
+    cmap=True,
+    style="c",
+    pen="gray10",
+)
+
+# Add legend for size-coding
+legend = io.StringIO(
+    "\n".join(f"S 0.4 c {0.01 * 2**m:.2f} - 1p 1 Mw {m}" for m in [4, 5, 6])
+)
+fig.legend(spec=legend, position="jBR+o0.2c+l2", box=True)
+
+with fig.inset(
+    position="jTL+w6c/3.5c+o0.1c",
+    margin=(1.2, 0.2, 1, 0.2),
+    box=pygmt.params.Box(fill="bisque"),
+):
+    fig.histogram(
+        region=[3.9, 7.1, 0, 0],
+        projection="X?/?",
+        frame=["WSrt", "xa1f0.1+lMw", "yaf+lCounts"],
+        data=df_eqs.mag,
+        series=0.1,
+        fill="darkgray",
+        pen="lightgray",
+        histtype=0,
+    )
+
+fig.show()
+fig.savefig("Fig4_PyGMT_pandas_myanmar.png")
