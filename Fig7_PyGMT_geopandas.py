c885c521b5b08addbebc81a589eb6d5fd50e82f9
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-19 20:10:46 +0100

    Add code for example covering points, lines, and polygons

--- Fig7_PyGMT_geopandas.py
+++ Fig7_PyGMT_geopandas.py
@@ -24,6 +24,40 @@ fig.savefig(fname="Fig7_PyGMT_geopandas.png")
 
 
 # %%
+import pygmt
+import geopandas as gpd
+
+world = gpd.read_file("https://naciscdn.org/naturalearth/50m/cultural/ne_50m_admin_0_countries.zip")
+rivers = gpd.read_file("https://naciscdn.org/naturalearth/110m/physical/ne_110m_rivers_lake_centerlines.zip")
+cities = gpd.read_file("https://naciscdn.org/naturalearth/110m/cultural/ne_110m_populated_places_simple.zip")
+
+world["POP_EST"] *= 1.0e-5
+
+fig = pygmt.Figure()
+fig.basemap(region="=SA", projection="M15c", frame=True)
+fig.plot(data=world, pen="0.5p,black", fill="lightgreen")
+pygmt.makecpt(cmap="turbo", series=(0, 3000, 100))
+fig.plot(
+    data=world,
+    pen="0.2p,gray10",
+    fill="+z",
+    cmap=True,
+    aspatial="Z=POP_EST",
+)
+fig.colorbar(frame=True)
+fig.plot(data=rivers, pen="1p,blue")
+fig.plot(data=cities, style="c0.1c", fill="red", pen="black")
+fig.text(
+    x=cities.geometry.x,
+    y=cities.geometry.y,
+    text=cities["name"],
+    font="10p,Helvetica-Bold,black",
+    offset="0.2c/0.2c",
+)
+fig.show()
+
+
+# %%
 import pandas as pd
 import pygmt
 
