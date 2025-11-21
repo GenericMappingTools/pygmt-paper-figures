31184023d422b4623ed72836baea10a4e55b3837
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-21 13:34:49 +0100

    Adjust annotations for y axis

--- Fig7_PyGMT_datetime.py
+++ Fig7_PyGMT_datetime.py
@@ -8,7 +8,7 @@ fig = pygmt.Figure()
 fig.basemap(
     projection="X12c/6c",
     region=[datetime.date(2016, 1, 1), datetime.date(2026, 12, 31), -50, 1000],
-    frame=["x+lYear", "y+lGitHub stars"],
+    frame=["x+lYear", "ya100f50+lGitHub stars"],
 )
 
 for csvfile, color, label in zip(
