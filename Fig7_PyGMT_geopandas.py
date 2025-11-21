8cb2e5898b747493f00dd4d326bdadbf7f573079
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-21 10:05:28 +0100

    Remove content for Cicago example

--- Fig7_PyGMT_geopandas.py
+++ Fig7_PyGMT_geopandas.py
@@ -1,37 +1,10 @@
-import geopandas as gpd
-import pygmt
-
-fig = pygmt.Figure()
-
-fig.basemap(region=[-88, -87.5, 41.64, 42.04], projection="M9c", frame="rtlb")
-
-gdf_airbnb = gpd.read_file("https://geodacenter.github.io/data-and-lab//data/airbnb.zip")
-popul_min = gdf_airbnb["population"].min()
-popul_max = gdf_airbnb["population"].max()
-
-pygmt.makecpt(cmap="SCM/bilbao", series=[popul_min, popul_max, 10])
-fig.colorbar(frame="x+lpopulation", position="jBL+jBL+o0.5c/0c+v")
-fig.plot(
-    data=gdf_airbnb,
-    pen="0.2p,gray10",
-    fill="+z",
-    cmap=True,
-    aspatial="Z=population",
-)
-
-fig.show()
-fig.savefig(fname="Fig7_PyGMT_geopandas.png")
-
-
-# %%
 import pygmt
 import geopandas as gpd
-
 
 world = gpd.read_file("https://naciscdn.org/naturalearth/50m/cultural/ne_50m_admin_0_countries.zip")
 rivers = gpd.read_file("https://naciscdn.org/naturalearth/110m/physical/ne_110m_rivers_lake_centerlines.zip")
 cities = gpd.read_file("https://naciscdn.org/naturalearth/110m/cultural/ne_110m_populated_places_simple.zip")
-cities_mega = cities[cities["megacity"]==1]
+cities_mega = cities[cities["megacity"]==1]  # Focus on large cities
 cities_world = cities[cities["worldcity"]==1]
 world["POP_EST"] *= 1.0e-5
 
