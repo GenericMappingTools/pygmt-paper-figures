d16a1d6996c24ef312578154de6cbe70bf6d062a
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-21 13:48:48 +0100

    Outcomment sst code example

--- Fig5_PyGMT_xarray.py
+++ Fig5_PyGMT_xarray.py
@@ -1,17 +1,17 @@
-import pygmt
-import xarray as xr
+# import pygmt
+# import xarray as xr
 
-url = "https://iridl.ldeo.columbia.edu/SOURCES/.NOAA/.NODC/.WOA09/.Grid-1x1/.Annual/.temperature/.t_an/data.nc"
-netcdf_file = pygmt.which(fname=url, download=True)
-woa_temp = xr.open_dataset(netcdf_file).isel(time=0)
+# url = "https://iridl.ldeo.columbia.edu/SOURCES/.NOAA/.NODC/.WOA09/.Grid-1x1/.Annual/.temperature/.t_an/data.nc"
+# netcdf_file = pygmt.which(fname=url, download=True)
+# woa_temp = xr.open_dataset(netcdf_file).isel(time=0)
 
-fig = pygmt.Figure()
-fig.basemap(region="d", projection="N10c", frame=True)
-fig.grdimage(grid=woa_temp.t_an.sel(depth=0), cmap="SCM/batlow")
-fig.colorbar(frame=["xaf+lSea surface temperature", "y+l@.C"])
+# fig = pygmt.Figure()
+# fig.basemap(region="d", projection="N10c", frame=True)
+# fig.grdimage(grid=woa_temp.t_an.sel(depth=0), cmap="SCM/batlow")
+# fig.colorbar(frame=["xaf+lSea surface temperature", "y+l@.C"])
 
-fig.show()
-fig.savefig(fname="Fig5_PyGMT_xarray_sst.png")
+# fig.show()
+# fig.savefig(fname="Fig5_PyGMT_xarray_sst.png")
 
 
 # %%
