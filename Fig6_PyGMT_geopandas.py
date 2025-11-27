645ff9c2556d044113bafdc520f5b98a7053e6af
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-27 22:06:07 +0100

    Adjust and shorten variable names

--- Fig6_PyGMT_geopandas.py
+++ Fig6_PyGMT_geopandas.py
@@ -7,7 +7,7 @@ world["POP_EST"] *= 1e-6
 rivers = gpd.read_file(f"{provider}/110m/physical/ne_110m_rivers_lake_centerlines.zip")
 cities = gpd.read_file(f"{provider}/110m/cultural/ne_110m_populated_places_simple.zip")
 cities_small = cities[cities["worldcity"] != 1].copy()  # Smaller cities
-cities_world = cities[cities["worldcity"] == 1].copy()  # Larger (world) cities
+cities_large = cities[cities["worldcity"] == 1].copy()  # Larger (world) cities
 
 fig = pygmt.Figure()
 fig.basemap(region=[-19.5, 54.5, -37.5, 37.5], projection="M15c", frame=True)
@@ -16,14 +16,14 @@ pygmt.makecpt(cmap="bilbao", series=(0, 200))
 fig.plot(data=world, pen="1p,gray50", fill="+z", cmap=True, aspatial="Z=POP_EST")
 fig.colorbar(frame="x20f10+lPopulation (millions)", position="jML+o3c/-3.5c+w7.5c+ml")
 
-fig.plot(data=rivers["geometry"], pen="1.5p,dodgerblue4")
+fig.plot(data=rivers, pen="1.5p,dodgerblue4")
 
 fig.plot(data=cities_small, style="s0.2c", fill="lightgray", pen="1p")
-fig.plot(data=cities_world, style="s0.3c", fill="darkorange", pen="1p")
+fig.plot(data=cities_large, style="s0.3c", fill="darkorange", pen="1p")
 fig.text(
-    x=cities_world.geometry.x,
-    y=cities_world.geometry.y,
-    text=cities_world["name"],
+    x=cities_large.geometry.x,
+    y=cities_large.geometry.y,
+    text=cities_large["name"],
     offset="0.2c/0.35c",
     justify="BL",
     font="10p,Helvetica-Bold",
@@ -44,32 +44,32 @@ import pygmt
 provider = "https://naciscdn.org/naturalearth"
 world = gpd.read_file(f"{provider}/50m/cultural/ne_50m_admin_0_countries.zip")
 world["POP_EST"] *= 1e-6
-world_africa = world[world["CONTINENT"] == "Africa"].copy()
+africa = world[world["CONTINENT"] == "Africa"].copy()
 rivers = gpd.read_file(f"{provider}/110m/physical/ne_110m_rivers_lake_centerlines.zip")
 cities = gpd.read_file(f"{provider}/110m/cultural/ne_110m_populated_places_simple.zip")
-cities_small = cities[cities["worldcity"] != 1].copy()  # Smaller cities
+cities_small = cities[cities["worldcity"] != 1].copy()
 cities_north = cities_small.cx[-19.5:14, 16:37].copy()
 cities_middle = cities_small.cx[-19.5:40, 15:16].copy()
 cities_south = cities_small.cx[:, -36.5:15].copy()
 cities_africa = pd.concat([cities_north, cities_south, cities_middle])
-cities_world = cities[cities["worldcity"] == 1].copy()  # Larger (world) cities
-cities_world = cities_world.cx[-19.5:40, -36.5:37].copy()
+cities_large = cities[cities["worldcity"] == 1].copy()
+cities_large = cities_large.cx[-19.5:40, -36.5:37].copy()
 
 fig = pygmt.Figure()
 fig.basemap(region=[-19.5, 52, -37, 38], projection="M15c", frame="+n")
 
 pygmt.makecpt(cmap="bilbao", series=(0, 200))
-fig.plot(data=world_africa, pen="0.8p,gray50", fill="+z", cmap=True, aspatial="Z=POP_EST")
+fig.plot(data=africa, pen="0.8p,gray50", fill="+z", cmap=True, aspatial="Z=POP_EST")
 fig.colorbar(frame="x20f10+lPopulation (millions)", position="jML+o3c/-3.5c+w7.5c+ml")
 
 fig.plot(data=rivers, pen="1.5p,dodgerblue4")
 
 fig.plot(data=cities_africa, style="s0.2c", fill="lightgray", pen="1p")
-fig.plot(data=cities_world, style="s0.3c", fill="darkorange", pen="1p")
+fig.plot(data=cities_large, style="s0.3c", fill="darkorange", pen="1p")
 fig.text(
-    x=cities_world.geometry.x,
-    y=cities_world.geometry.y,
-    text=cities_world["name"],
+    x=cities_large.geometry.x,
+    y=cities_large.geometry.y,
+    text=cities_large["name"],
     offset="0.2c/0.35c",
     justify="BL",
     font="10p,Helvetica-Bold",
