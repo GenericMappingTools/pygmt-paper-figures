ad7a1bdbe711281aba68d384d1cbe53baf9ffd94
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-27 11:38:04 +0100

    Adjust provider | Fix geopandas warning | Use full line length

--- Fig6_PyGMT_geopandas.py
+++ Fig6_PyGMT_geopandas.py
@@ -1,26 +1,23 @@
 import geopandas as gpd
 import pygmt
 
-provider = "https://naciscdn.org/naturalearth/"
-world = gpd.read_file(f"{provider}50m/cultural/ne_50m_admin_0_countries.zip")
+provider = "https://naciscdn.org/naturalearth"
+world = gpd.read_file(f"{provider}/50m/cultural/ne_50m_admin_0_countries.zip")
 world["POP_EST"] *= 1e-6
 rivers = gpd.read_file(f"{provider}110m/physical/ne_110m_rivers_lake_centerlines.zip")
 cities = gpd.read_file(f"{provider}110m/cultural/ne_110m_populated_places_simple.zip")
-cities_small = cities[cities["worldcity"] != 1]  # Smaller cities
-cities_world = cities[cities["worldcity"] == 1]  # Larger (world) cities
+cities_small = cities[cities["worldcity"] != 1].copy()  # Smaller cities
+cities_world = cities[cities["worldcity"] == 1].copy()  # Larger (world) cities
 
 fig = pygmt.Figure()
 fig.basemap(region=[-19.5, 53, -38, 37.5], projection="M15c", frame=True)
+
 pygmt.makecpt(cmap="bilbao", series=(0, 100))
-fig.plot(
-    data=world,
-    pen="1p,gray50",
-    fill="+z",
-    cmap=True,
-    aspatial="Z=POP_EST",
-)
+fig.plot(data=world, pen="1p,gray50", fill="+z", cmap=True, aspatial="Z=POP_EST")
 fig.colorbar(frame="x10f5+lPopulation (millions)", position="+ef0.3c")
+
 fig.plot(data=rivers, pen="1.5p,darkblue")
+
 fig.plot(data=cities_small, style="s0.2c", fill="lightgray", pen="1p")
 fig.plot(data=cities_world, style="s0.3c", fill="darkorange", pen="1p")
 fig.text(
@@ -34,5 +31,6 @@ fig.text(
     pen="0.8p,darkorange",
     clearance="0.1c+tO",
 )
+
 fig.show()
 fig.savefig(fname="Fig6_PyGMT_geopandas.png")
