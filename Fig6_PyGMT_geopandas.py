74bcc3a308251c0e66ca462118660ee29aaaedcd
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-27 17:22:12 +0100

    TEMPORALY add code for focus on Africa

--- Fig6_PyGMT_geopandas.py
+++ Fig6_PyGMT_geopandas.py
@@ -34,3 +34,51 @@ fig.text(
 
 fig.show()
 fig.savefig(fname="Fig6_PyGMT_geopandas.png")
+
+
+# %%
+import geopandas as gpd
+import pandas as pd
+import pygmt
+
+provider = "https://naciscdn.org/naturalearth"
+world = gpd.read_file(f"{provider}/50m/cultural/ne_50m_admin_0_countries.zip")
+world["POP_EST"] *= 1e-6
+world_africa = world[world["CONTINENT"] == "Africa"].copy()
+rivers = gpd.read_file(f"{provider}/50m/physical/ne_50m_rivers_lake_centerlines.zip")
+rivers = rivers[rivers["scalerank"] != 5]
+rivers = rivers.cx[-19.5:40, -36.5:30].copy()
+cities = gpd.read_file(f"{provider}/110m/cultural/ne_110m_populated_places_simple.zip")
+cities_small = cities[cities["worldcity"] != 1].copy()  # Smaller cities
+cities_small_north = cities_small.cx[-19.5:14, 16:37].copy()
+cities_small_middle = cities_small.cx[-19.5:40, 15:16].copy()
+cities_small_south = cities_small.cx[:, -36.5:15].copy()
+cities_small_africa = pd.concat([cities_small_north, cities_small_south, cities_small_middle])
+cities_world = cities[cities["worldcity"] == 1].copy()  # Larger (world) cities
+cities_world = cities_world.cx[-19.5:40, -36.5:37].copy()
+
+fig = pygmt.Figure()
+fig.basemap(region=[-19.5, 52, -37, 38], projection="M15c", frame="+n")
+
+pygmt.makecpt(cmap="bilbao", series=(0, 200))
+fig.plot(data=world_africa[["geometry", "POP_EST"]], pen="0.8p,gray50", fill="+z", cmap=True, aspatial="Z=POP_EST")
+fig.colorbar(frame="x20f10+lPopulation (millions)", position="jML+o3c/-3.5c+w7.5c+ml")
+
+fig.plot(data=rivers[["geometry"]], pen="1.5p,dodgerblue4")
+
+fig.plot(data=cities_small_africa[["geometry"]], style="s0.2c", fill="lightgray", pen="1p")
+fig.plot(data=cities_world[["geometry"]], style="s0.3c", fill="darkorange", pen="1p")
+fig.text(
+    x=cities_world.geometry.x,
+    y=cities_world.geometry.y,
+    text=cities_world["name"],
+    offset="0.2c/0.35c",
+    justify="BL",
+    font="10p,Helvetica-Bold",
+    fill="white@30",
+    pen="0.8p,darkorange",
+    clearance="0.1c+tO",
+)
+
+fig.show()
+fig.savefig(fname="Fig6_PyGMT_geopandas_africa.png")
