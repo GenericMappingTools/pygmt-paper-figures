7bd7935f381d053fc8dd729c9b1e84e3fc6169d1
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-28 08:21:29 +0100

    Use 110 m also for population

--- Fig6_PyGMT_geopandas.py
+++ Fig6_PyGMT_geopandas.py
@@ -41,7 +41,7 @@ import geopandas as gpd
 import pygmt
 
 provider = "https://naciscdn.org/naturalearth"
-world = gpd.read_file(f"{provider}/50m/cultural/ne_50m_admin_0_countries.zip")
+world = gpd.read_file(f"{provider}/110m/cultural/ne_110m_admin_0_countries.zip")
 world["POP_EST"] *= 1e-6
 africa = world[world["CONTINENT"] == "Africa"].copy()
 rivers = gpd.read_file(f"{provider}/110m/physical/ne_110m_rivers_lake_centerlines.zip")
