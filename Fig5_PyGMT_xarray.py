b65e807fb8b22e8902d0ab11ade7bd5edab3884a
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-29 21:47:22 +0100

    Shorten variable names

--- Fig5_PyGMT_xarray.py
+++ Fig5_PyGMT_xarray.py
@@ -3,25 +3,17 @@ from pygmt.datasets import load_mars_relief
 
 mars = load_mars_relief(resolution="10m")
 with pygmt.config(PROJ_ELLIPSOID="mars"):
-    mars_filtered = mars.gmt.filter(filter="g50", distance=4)
-    mars_gradient = mars_filtered.gmt.gradient(azimuth=-45, normalize="t1")
+    mars_filter = mars.gmt.filter(filter="g50", distance=4)
+    mars_grad = mars_filter.gmt.gradient(azimuth=-45, normalize="t1")
 
 fig = pygmt.Figure()
 pygmt.makecpt(cmap="batlow", series=[-6500, 6500])
 fig.grdimage(
-    mars_filtered,
-    cmap=True,
-    shading=mars_gradient,
-    projection="G90/-20/12c",
-    frame="g30",
+    mars_filter, cmap=True, shading=mars_grad, projection="G90/-20/12c", frame="g30",
 )
 fig.shift_origin(xshift="w+0.5c")
 fig.grdimage(
-    mars_filtered,
-    cmap=True,
-    shading=mars_gradient,
-    projection="G270/20/12c",
-    frame="g30",
+    mars_filter, cmap=True, shading=mars_grad, projection="G270/20/12c", frame="g30",
 )
 fig.colorbar(
     frame=["xa2000f1000+lElevation of Mars", "y+lm"], position="+e0.3c+o-6.25c/0.2c+ml"
