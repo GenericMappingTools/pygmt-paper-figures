1653b66c2f4baba73b9d69de9e7391b31ff10cd2
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-11-30 10:54:06 +0100

    Add codes for Chicago map

--- Fig6_PyGMT_geopandas.py
+++ Fig6_PyGMT_geopandas.py
@@ -1,3 +1,56 @@
+import geodatasets
+import geopandas as gpd
+import pygmt
+
+gdf = gpd.read_file(geodatasets.get_path("geoda airbnb"))
+
+provider = "https://naciscdn.org/naturalearth"
+cities = gpd.read_file(f"{provider}/10m/cultural/ne_10m_populated_places_simple.zip")
+cities = cities.cx[-87.94:-87.52, 41.64:42.02]
+airports = gpd.read_file(f"{provider}/10m/cultural/ne_10m_airports.zip")
+airports = airports.cx[-87.94:-87.52, 41.64:42.02]
+ports = gpd.read_file(f"{provider}/10m/cultural/ne_10m_ports.zip")
+ports = ports.cx[-87.94:-87.52, 41.64:42.02]
+railroads = gpd.read_file(f"{provider}/10m/cultural/ne_10m_railroads.zip")
+railroads = railroads.cx[-87.94:-87.52, 41.64:42.02]
+# rivers = gpd.read_file(f"{provider}/10m/physical/ne_10m_rivers_lake_centerlines.zip")
+# rivers = rivers.cx[-87.94:-87.52, 41.64:42.02]
+
+# %%
+fig = pygmt.Figure()
+fig.basemap(region=gdf.total_bounds[[0, 2, 1, 3]], projection="M10c", frame="+n")
+fig.coast(shorelines=True, rivers="1/1p,blue")
+
+pygmt.makecpt(cmap="bilbao", series=[gdf["population"].min(), gdf["population"].max()])
+fig.plot(data=gdf, pen="0.3p,gray10", fill="+z", cmap=True, aspatial="Z=population")
+fig.colorbar(frame="xaf+lPopulation in Chicago", position="jTR+o0.8c/0.3c+w6c+ml")
+
+fig.plot(data=railroads["geometry"], pen="2p,black")
+fig.plot(data=railroads["geometry"], pen="1p,white,2_2")
+
+# fig.plot(data=rivers["geometry"], pen="1p,steelblue")
+
+fig.plot(data=ports["geometry"], style="i0.35c", fill="steelblue", pen="0.8p,gray10", label="port")
+fig.plot(data=airports["geometry"], style="t0.35c", fill="darkorange", pen="0.8p,gray10", label="airport")
+fig.text(
+    x=airports.geometry.x,
+    y=airports.geometry.y,
+    text=airports["name"],
+    offset="0c/-0.25c",
+    justify="TL",
+    font="8p,Helvetica-Bold",
+    fill="white@30",
+    pen="0.5p,darkorange",
+    clearance="0.08c+tO",
+)
+fig.plot(data=cities["geometry"], style="s0.3c", fill="red", pen="0.8p,gray10", label="city")
+fig.legend(position="jBL+o0.2c+l1.5", box=True)
+
+fig.show()
+fig.savefig(fname="Fig6_PyGMT_geopandas_chicago.png")
+
+
+# %%
 import geopandas as gpd
 import pygmt
 
@@ -35,4 +88,4 @@ fig.text(
 )
 
 fig.show()
-fig.savefig(fname="Fig6_PyGMT_geopandas.png")
+fig.savefig(fname="Fig6_PyGMT_geopandas_africa.png")
