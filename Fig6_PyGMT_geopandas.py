a8112843a320664506ed7e3cd9b9926b88b367ea
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2025-12-02 12:40:06 +0100

    Select rivers and cities located in USA
    Co-authored-by: Dongdong Tian <seisman.info@gmail.com>

--- Fig6_PyGMT_geopandas.py
+++ Fig6_PyGMT_geopandas.py
@@ -2,13 +2,14 @@ import geopandas as gpd
 import pygmt
 
 provider = "https://naciscdn.org/naturalearth"
-states = gpd.read_file(f"{provider}/110m/cultural/ne_110m_admin_1_states_provinces.zip")
-states["area_sqkm"] = states.geometry.to_crs(epsg=6933).area / 10 ** 9
-states = states[states["name"] != "Alaska"]
-rivers = gpd.read_file(f"{provider}/110m/physical/ne_110m_rivers_lake_centerlines.zip")
-rivers = rivers.cx[-126:-66, 24.9:49.1]
+states = gpd.read_file(f"{provider}/50m/cultural/ne_50m_admin_1_states_provinces.zip")
+rivers = gpd.read_file(f"{provider}/50m/physical/ne_50m_rivers_lake_centerlines.zip")
 cities = gpd.read_file(f"{provider}/110m/cultural/ne_110m_populated_places_simple.zip")
-cities = cities.cx[-126:-66, 25.7:42]
+
+states = states[states["admin"] == "United States of America"].copy()
+states["area_sqkm"] = states.geometry.to_crs(epsg=6933).area / 10 ** 9
+rivers = rivers[rivers.intersects(states.union_all())].copy()
+cities = cities[cities["adm0name"] == "United States of America"].copy()
 
 fig = pygmt.Figure()
 fig.basemap(projection="L-96/35/33/41/12c", region=[-126, -66, 25, 49], frame="+n")
